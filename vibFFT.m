51af2b69f6d84ddc52c6227fd2f2f10617aedeb1
ilene97 <ilene97@naver.com>
2021-04-08 22:18:15 +0900

    signal cut all axis

--- vibFFT.m
+++ vibFFT.m
@@ -1,9 +1,10 @@
 function [output] = vibFFT(data)
     [~, idx] = max(abs(data));
-    data = circshift(data, ceil(length(data) / 2) - idx);
+    %data = circshift(data, ceil(length(data) / 2) - idx);
     
     win = hamming(length(data));    
     temp = abs(fft(data .* win')); 
-    temp = temp(2:ceil(length(temp)/2) + 1);
+    temp = temp(2:floor(length(temp)/2) + 1);
     output = temp;
+    
 end
